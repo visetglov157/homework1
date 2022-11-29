a7719aad2c289cc1c8d04266d7959c9e10e757ff
Vladislav <vladislav33lavrov@gmail.com>
2022-11-29 21:20:32 +0300

    add file gitignore and exercise_2

---
+++ exercise_2/Program.cs
@@ -0,0 +1,29 @@
+﻿Console.Clear();
+Console.Write("enter a: ");
+int a = int.Parse(Console.ReadLine());
+Console.Write("enter b: ");
+int b = int.Parse(Console.ReadLine());
+Console.Write("enter c: ");
+int c = int.Parse(Console.ReadLine());
+if (a > b)
+{
+    if (a > c)
+    {
+        Console.WriteLine("max number: " + a);
+    }
+    else
+    {
+        Console.WriteLine("max number: " + c);
+    }
+}
+
+else if (b > c)
+{
+    Console.WriteLine("max number: " + b);
+}
+else
+{
+    Console.WriteLine("max number: " + c);
+}
+
+
