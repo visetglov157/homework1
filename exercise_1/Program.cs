418a80b7d9b068d07a65b068c9d9554046965774
Vladislav <vladislav33lavrov@gmail.com>
2022-11-29 20:25:56 +0300

    add commit Console.WriteLine("enter a: ");

---
+++ exercise_1/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Clear();
+Console.Write("enter a: ");
+int a = int.Parse(Console.ReadLine());
+Console.Write("enter b: ");
+int b = int.Parse(Console.ReadLine());
+if(a<b)
+{
+    Console.Write("max = ");
+    Console.WriteLine(b);
+}
+else
+{
+    Console.Write("max = ");
+    Console.WriteLine(a);
+}
+
+
+
