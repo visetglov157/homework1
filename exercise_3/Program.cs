37e2cd25a5167b80d33f0a4ecc76cb56a07fb9a3
Vladislav <vladislav33lavrov@gmail.com>
2022-11-29 21:32:55 +0300

    add exercise_3

---
+++ exercise_3/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Clear();
+Console.Write("enter number: ");
+int number = int.Parse(Console.ReadLine());
+if(number % 2 == 1 )
+{
+    Console.WriteLine("нечетное число");
+}
+else
+{
+    Console.WriteLine("четное число");
+}
